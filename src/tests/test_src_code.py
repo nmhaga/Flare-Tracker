dcce84a20969ad87ebed59f2fe5b7828d6a3713b
Nomajama Mhaga <nmhaga@sansa.org.za>
2014-09-12 16:25:46 +0200

    added xrayflux tests to check for duplicates with fake data

--- src/tests/test_src_code.py
+++ src/tests/test_src_code.py
@@ -1,10 +1,10 @@
-import datetime
+from datetime import datetime
 
 import sys
 sys.path.append('../') # but pytest have to be called from test directory
 # That also affect to the 'sample_url.html' file below.
 import src_code
-import swp_database
+from swp_database import Solarsoft, Xrayflux
 import unittest
 
 class test_src_code(unittest.TestCase):
@@ -12,48 +12,63 @@ class test_src_code(unittest.TestCase):
         self.assertTrue(True)
         
     def test_read_solarsoft_data(self):
-        #TODO: change assertTrue to assertEqual where applicable. 
         with open('sample_url.html', 'r') as sample_file:
             result = src_code.read_solarsoft_data(sample_file)
+        
         # Test length
-        self.assertTrue(len(result) == 6)
+        self.assertEqual(len(result), 6)
+        
         # Test start time
-        self.assertTrue(result[0][0] == datetime.datetime(2014,9,2,0,8,0))
-        self.assertTrue(result[-1][0] == datetime.datetime(2014,9,2,20,56,0))
+        Start_dt1 = datetime(2014,9,2,0,8,0)
+        Start_dt2 = datetime(2014,9,2,20,56,0)
+        self.assertEqual(result[0].ut_datetime, Start_dt1)
+        self.assertEqual(result[-1].ut_datetime, Start_dt2)
+        
         # Test Peak
-        self.assertTrue(result[0][1] == datetime.datetime(2014,9,2,0,16,0))
-        self.assertTrue(result[1][1] == datetime.datetime(2014,9,2,0,16,0))
-        # Test GOES_flare
-        self.assertTrue(result[0][2] == src_code.convert_flare_format_into_decimal('C2.3'))
-        self.assertTrue(result[2][2] == src_code.convert_flare_format_into_decimal('C2.6'))
+        peak_dt1 = datetime(2014,9,2,0,16,0)
+        peak_dt2 = datetime(2014,9,2,0,16,0)
+        self.assertEqual(result[0].peak, peak_dt1)
+        self.assertEqual(result[1].peak, peak_dt2)
+ 
         # Test DerivedPosition
-        #TODO: fix the code so that we don't need the .strip()
-        self.assertEqual(result[0][3].strip(), 'N06W80')
-        self.assertEqual(result[3][3].strip(), 'S14W05')
+        expectedPos1 = 'N06W80'
+        expectedPos2 = 'S14W05'
+        self.assertEqual(result[0].derived_position, expectedPos1)
+        self.assertEqual(result[3].derived_position, expectedPos2)
+        
         # Test NewR
-        self.assertTrue(result[0][4] == '2149')
-        self.assertTrue(result[4][4] == '2152')
+        expectedReg1 = '2149'
+        expectedReg2 = '2152'
+        self.assertEqual(result[0].region, expectedReg1)
+        self.assertEqual(result[4].region, expectedReg2)
+    
         
     def test_get_peakdate_from_startdate(self):
-        self.assertTrue( src_code.get_peakdate_from_startdate(start = "2014/01/01 00:00:00", peak = "00:06:00") == datetime.datetime(2014, 1, 1, 0,6,0))
-        self.assertTrue( src_code.get_peakdate_from_startdate(start = "2013/12/31 23:58:50", peak = "00:06:00") == datetime.datetime(2014, 1, 1, 0,6,0))
+        start_value = "2013/12/31 23:58:50"
+        peak_value = "00:06:00"
+        expected = datetime(2014, 1, 1, 0,6,0)
+        self.assertEqual(src_code.get_peakdate_from_startdate(start=start_value, peak=peak_value), expected)
              
     def test_generate_filename(self):
-        self.assertTrue(src_code.generate_filename(date = datetime.datetime(2014,9,8), cadence=1) == "20140908_Gp_xr_1m.txt")
+        self.assertEqual(src_code.generate_filename(date=datetime(2014,9,8), cadence=1), "20140908_Gp_xr_1m.txt")
         
-        self.assertTrue(src_code.generate_filename(date = datetime.datetime(2014,9,8), cadence=5) == "20140908_Gp_xr_5m.txt")
+        self.assertEqual(src_code.generate_filename(date=datetime(2014,9,8), cadence=5), "20140908_Gp_xr_5m.txt")
         
     #test this method: goes_flare = convert_flare_format_into_decimal(goes_class)
-
-
-class test_database_parts_of_src_code(unittest.TestCase):
+    def test_convert_flare_format_into_decimal(self):
+        testvalue = src_code.convert_flare_format_into_decimal('C2.3')
+        expectedvalue = 2.2999999999999996e-06
+        
+        self.assertEqual(testvalue, expectedvalue)
+     
+class test_solarsoft_database_parts_of_src_code(unittest.TestCase):
      
     def setUp(self): 
         self.session = src_code.initialise_database()
-        self.dt = datetime.datetime(1990, 9, 19, 9, 19)
+        self.dt = datetime(1990, 9, 19, 9, 19)
         
     def tearDown(self): #takes stuff out of the database. :) 
-        res = self.session.query(swp_database.Solarsoft).filter(swp_database.Solarsoft.ut_datetime == self.dt).all()
+        res = self.session.query(Solarsoft).filter(Solarsoft.ut_datetime == self.dt).all()  
         for item in res: 
             self.session.delete(item)
         self.session.commit()
@@ -62,18 +77,51 @@ class test_database_parts_of_src_code(unittest.TestCase):
         self.assertTrue(True)     
         
     def test_fake_data_isnt_duplicated_in_database(self):
-        resultset = [(self.dt, self.dt, 0.0001, "N19W19", 1919)]
+        resultset = [Solarsoft(ut_datetime=self.dt, peak=self.dt, goes_class=0.0001, derived_position="N19W19", region=1919)]
         src_code.insert_solarsoft_data(resultset, self.session)    
         src_code.insert_solarsoft_data(resultset, self.session)
-        res = self.session.query(swp_database.Solarsoft).filter(swp_database.Solarsoft.ut_datetime == self.dt).all()
+        res = self.session.query(Solarsoft).filter(Solarsoft.ut_datetime == self.dt).all()
         
         self.assertEqual(len(res), 1)
     
     def test_fake_data_isnt_duplicated_in_database_even_if_in_same_list(self):
-        result = (self.dt, self.dt, 0.0001, "N19W19", 1919)
+    
+        result = Solarsoft(ut_datetime=self.dt, peak=self.dt, goes_class=0.0001, derived_position="N19W19", region=1919)
         resultset = [result,result] 
         src_code.insert_solarsoft_data(resultset, self.session)
-        res = self.session.query(swp_database.Solarsoft).filter(swp_database.Solarsoft.ut_datetime == self.dt).all()
+        res = self.session.query(Solarsoft).filter(Solarsoft.ut_datetime == self.dt).all()
+        
+        self.assertEqual(len(res), 1)
+
+class test_XrayFlux_database_parts_of_src_code(unittest.TestCase):
+     
+    def setUp(self): 
+        self.session = src_code.initialise_database()
+        self.dt = datetime(1990, 9, 19, 9, 19)
+        
+    def tearDown(self): #takes stuff out of the database. :) 
+        res = self.session.query(Xrayflux).filter(Xrayflux.ut_datetime == self.dt).all()  
+        for item in res: 
+            self.session.delete(item)
+        self.session.commit()
+        
+    def test_True(self):
+        self.assertTrue(True)     
+        
+    def test_fake_XR_data_isnt_duplicated_in_database(self):
+        resultset = [Xrayflux(ut_datetime=self.dt, longx=float(9.79e-07), short=float(1.39e-08))]
+        src_code.insert_xrayflux_data(resultset, self.session)    
+        src_code.insert_xrayflux_data(resultset, self.session)
+        res = self.session.query(Xrayflux).filter(Xrayflux.ut_datetime == self.dt).all()
+        
+        self.assertEqual(len(res), 1)
+    
+    def test_fake_XR_data_isnt_duplicated_in_database_even_if_in_same_list(self):
+    
+        result = Xrayflux(ut_datetime=self.dt, longx=float(9.79e-07), short=float(1.39e-08))
+        resultset = [result,result] 
+        src_code.insert_xrayflux_data(resultset, self.session)
+        res = self.session.query(Xrayflux).filter(Xrayflux.ut_datetime == self.dt).all()
         
         self.assertEqual(len(res), 1)
         
